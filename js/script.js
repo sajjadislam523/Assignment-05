f1086ba56a6174b1c705e2113467aa83db9cab75
sajjadislam523 <sajjad.islam523@gmail.com>
2024-09-23 00:53:27 +0600

    Created a global function to calculate donation

--- js/script.js
+++ js/script.js
@@ -1,24 +1,76 @@
-
 // global function
-function getInputValueById(id) {
-    const inputField = document.getElementById(id);
-    const inputFieldValue = parseFloat(inputField.value);
-    inputField.value = "";
-    return inputFieldValue;
+function getInputValueById(id, isTextElement = false) {
+    const element = document.getElementById(id);
+
+    const value = isTextElement ? parseFloat(element.innerText) : parseFloat(element.value);
+
+    if (!isTextElement) {
+        element.value = "";
+    }
+
+    return isNaN(value) ? 0 : value;
 }
 
+const historyButton = document.getElementById("display-history");
+const donationButton = document.getElementById("display-donation");
+
+document.getElementById("display-history").addEventListener("click", function () {
+    document.getElementById("donation").classList.add("hidden");
+    document.getElementById("history").classList.remove("hidden");
+
+    historyButton.classList.add("bg-[#B4F461]", "font-semibold", "text-black", "font-lexend");
+    donationButton.classList.remove("bg-[#B4F461]", "font-semibold", "text-black", "font-lexend");
+
+});
+
+document.getElementById("display-donation").addEventListener("click", function () {
+    document.getElementById("history").classList.add("hidden");
+    document.getElementById("donation").classList.remove("hidden");
+
+    historyButton.classList.remove("bg-[#B4F461]", "font-semibold", "text-black", "font-lexend");
+    donationButton.classList.add("bg-[#B4F461]", "font-semibold", "text-black", "font-lexend");
+});
+
+
+function handleDonation(dontionInputId, donationAmountId, donationNameId, totalDonationId, modalId) {
+    const donationValue = getInputValueById(dontionInputId);
+
+    if (donationValue <= 0) {
+        alert("Please enter a valid donation amount.");
+        return;
+    }
+
+    document.getElementById(modalId).showModal();
+
+    const donationAmount = getInputValueById(donationAmountId, true);
+    document.getElementById(donationAmountId).innerText = donationAmount + donationValue;
+
+    const totalDonation = getInputValueById(totalDonationId, true);
+    document.getElementById(totalDonationId).innerText = totalDonation >= donationValue
+        ? totalDonation - donationValue
+        : 0;
+
+    const history = document.getElementById("history");
+    const donationName = document.getElementById(donationNameId);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="p-8 border rounded-2xl my-4">
+        <h1 class="text-xl font-bold text-black font-lexend">${donationValue} Taka is Donated for ${donationName.innerText}</h1>
+        <p>Date: ${new Date().toLocaleDateString()}</p>
+    </div>
+    `;
+    history.insertBefore(div, history.firstChild);
+}
 
 document.getElementById("noakhali-donate").addEventListener("click", function () {
+    handleDonation("noakhali-donation", "noakhali-donation-amount", "noakhali-name", "total-donation", "my_modal_5");
+})
 
-    const noakhaliValue = getInputValueById("noakhali-donation");
+document.getElementById("feni-donate").addEventListener("click", function () {
+    handleDonation("feni-donation", "feni-donation-amount", "feni-name", "total-donation", "my_modal_5");
+})
 
-    document.getElementById("my_modal_5").showModal();
-    const noakhaliDonation = document.getElementById("noakhali-donation-amount");
-    const noakhaliDonationValue = parseFloat(noakhaliDonation.innerText);
-    noakhaliDonation.innerText = noakhaliDonationValue + noakhaliValue;
 
-    const totalDonation = document.getElementById("total-donation");
-    const totalDonationValue = parseFloat(totalDonation.innerText);
-    totalDonation.innerText = totalDonationValue - noakhaliValue;
-
+document.getElementById("quota-donate").addEventListener("click", function () {
+    handleDonation("quota-donation", "quota-donation-amount", "quota-name", "total-donation", "my_modal_5");
 })
